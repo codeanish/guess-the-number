47583872015e03026ebf43d833e9623466694cb3
codeanish <anish@codeanish.com>
2022-12-03 09:25:25 +0000

    Fixing mobile nav

--- src/components/ConfiguratorBar.tsx
+++ src/components/ConfiguratorBar.tsx
@@ -15,19 +15,36 @@ const ConfiguratorBar = (props: Props) => {
     }
 
     return (
-        <div className="flex flex-row space-x-4 bg-neutral-800 opacity-75 pt-1 pb-1 px-2 py-2 text-sm rounded-md">
-            <IconButton icon={<Plus/>} text="add" isActive={true}/>
-            <IconButton icon={<Minus/>} text="subtract" isActive={false}/>
-            <IconButton icon={<X/>} text="multiply" isActive={false}/>
-            <IconButton icon={<Divide/>} text="divide" isActive={false}/>
-        
-        <div className='bg-neutral-700 w-1 rounded-sm'></div>
-            
-            <TextButton isActive={props.selectedTime == 15} text="15" onClick={() => HandleTimeButtonSelected(15)}/>
-            <TextButton isActive={props.selectedTime == 30} text="30" onClick={() => HandleTimeButtonSelected(30)}/>
-            <TextButton isActive={props.selectedTime == 60} text="60" onClick={() => HandleTimeButtonSelected(60)}/>
-            <TextButton isActive={props.selectedTime == 120} text="120" onClick={() => HandleTimeButtonSelected(120)}/>
+        <div>
+            <div className="hidden md:flex flex-row space-x-4 bg-neutral-800 opacity-75 pt-1 pb-1 px-2 py-2 text-sm rounded-md">
+                    <IconButton icon={<Plus/>} text="add" isActive={true}/>
+                    <IconButton icon={<Minus/>} text="subtract" isActive={false}/>
+                    <IconButton icon={<X/>} text="multiply" isActive={false}/>
+                    <IconButton icon={<Divide/>} text="divide" isActive={false}/>
+                    <div className='bg-neutral-700 w-1 rounded-sm'></div>
+                    <TextButton isActive={props.selectedTime == 15} text="15" onClick={() => HandleTimeButtonSelected(15)}/>
+                    <TextButton isActive={props.selectedTime == 30} text="30" onClick={() => HandleTimeButtonSelected(30)}/>
+                    <TextButton isActive={props.selectedTime == 60} text="60" onClick={() => HandleTimeButtonSelected(60)}/>
+                    <TextButton isActive={props.selectedTime == 120} text="120" onClick={() => HandleTimeButtonSelected(120)}/>
+            </div>
+            <div className="md:hidden flex flex-col space-y-2 bg-neutral-800 opacity-75 pt-1 pb-1 px-2 py-2 text-sm rounded-md">
+                    <div className="flex flex-row justify-evenly space-x-4">
+                        <IconButton icon={<Plus/>} text="add" isActive={true}/>
+                        <IconButton icon={<Minus/>} text="subtract" isActive={false}/>
+                        <IconButton icon={<X/>} text="multiply" isActive={false}/>
+                        <IconButton icon={<Divide/>} text="divide" isActive={false}/>
+                    </div>
+                    <div className='bg-neutral-700 h-1 rounded-sm'></div>
+                    <div className='flex flex-row justify-evenly'>
+                        <TextButton isActive={props.selectedTime == 15} text="15" onClick={() => HandleTimeButtonSelected(15)}/>
+                        <TextButton isActive={props.selectedTime == 30} text="30" onClick={() => HandleTimeButtonSelected(30)}/>
+                        <TextButton isActive={props.selectedTime == 60} text="60" onClick={() => HandleTimeButtonSelected(60)}/>
+                        <TextButton isActive={props.selectedTime == 120} text="120" onClick={() => HandleTimeButtonSelected(120)}/>
+                    </div>
+            </div>
         </div>
+
+
   )
 }
 
